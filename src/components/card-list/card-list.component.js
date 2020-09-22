270dac4a25e8659c26093bf04d2f2988e209ed04
Nelle <nellebreyes@gmail.com>
2020-09-22 10:38:03 -0400

    complete search robots app

---
+++ src/components/card-list/card-list.component.js
@@ -0,0 +1,16 @@
+import React from "react";
+import "./card-list.styles.css";
+
+import { Card } from "../card/card.component";
+
+const CardList = (props) => {
+  return (
+    <div className="card-list">
+      {props.monsters.map((monster, id) => (
+        <Card key={id} monster={monster} />
+      ))}
+    </div>
+  );
+};
+
+export default CardList;
